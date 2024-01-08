d01fff61ef0077a40b0fceb789fb17c27df59190
Deepti Shakya <90705308+deeptishakya22@users.noreply.github.com>
2024-01-08 21:38:07 +0530

    Updated Solution - GfG to GitHub

--- Medium/Subarray with 0 sum/subarray-with-0-sum.cpp
+++ Medium/Subarray with 0 sum/subarray-with-0-sum.cpp
@@ -13,17 +13,16 @@ class Solution{
     //Function to check whether there is a subarray present with 0-sum or not.
     bool subArrayExists(int arr[], int n)
     {
-        //Your code here
+        unordered_set<int> sumSet;
+        int sum = 0;
         for (int i = 0; i < n; i++) {
-            int sum = arr[i];
-            if (sum == 0)
+            sum += arr[i];
+
+            if (sum == 0 || sumSet.find(sum) != sumSet.end())
                 return true;
-        for (int j = i + 1; j < n; j++) {
-            sum += arr[j];
-            if (sum == 0)
-                return true;
+ 
+            sumSet.insert(sum);
         }
-    }
     return false;
     }
 };
