89a3b4484bae07e06f38c0a5d9af0799a0337f90
Deepti Shakya <90705308+deeptishakya22@users.noreply.github.com>
2024-01-17 19:50:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Index Of an Extra Element/index-of-an-extra-element.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+  public:
+    int findExtra(int a[], int b[], int n) {
+        for(int i=0, j=0; i<n, j<n; i++, j++) {
+            if(a[i] != b[j]) return j;
+        }
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        int a[n], b[n - 1];
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+        }
+        for (int i = 0; i < n - 1; i++) {
+            cin >> b[i];
+        }
+        Solution obj;
+        cout << obj.findExtra(a, b, n) << endl;
+    }
+}
+// } Driver Code Ends
