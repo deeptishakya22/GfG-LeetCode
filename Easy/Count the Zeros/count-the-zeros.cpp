ad143c76e289214d26dd83fef4b1aba9b06604ef
Deepti Shakya <90705308+deeptishakya22@users.noreply.github.com>
2024-04-24 23:38:11 +0530

    Updated Solution - GfG to GitHub

--- Easy/Count the Zeros/count-the-zeros.cpp
+++ Easy/Count the Zeros/count-the-zeros.cpp
@@ -10,13 +10,13 @@ using namespace std;
 class Solution{   
 public:
     int countZeroes(int arr[], int n) {
-        int count = 0;
-        for(int i=0; i<n; i++) {
-            if(arr[i]==0) {
-                count++;
-            }
+        int counter = 0;
+        for(int i=0; i < n; i++)
+        {
+            if(arr[i] == 0)
+                counter++;
         }
-        return count;
+        return counter; 
     }
 };
 
